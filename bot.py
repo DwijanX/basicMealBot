5ddb86adff559f406dd1a7437764da492500dcdf
dwijan <christianriveroarnez@gmail.com>
2024-04-23 00:21:00 -0400

    Refactor bot.py to schedule and send daily recipe messages

--- bot.py
+++ bot.py
@@ -1,25 +1,28 @@
 import asyncio
-from telegram import Bot
-import schedule
 import time
+from telegram import Bot
 from apiRequest.api_request import generate_recipe
 from dotenv import dotenv_values
+import schedule
+
 config = dotenv_values(".env")
 bot = Bot(token=config.bot_token)
 chat_id = config.chat_id
 plate = 'Pollo a la parmesana'
 
 async def send_daily_message():
-    
     recipe = generate_recipe(plate)
-    print(recipe)
-    message=plate + ":\nUna idea para preparar es:" + recipe
+    message = f"{plate}:\nUna idea para preparar es:\n{recipe}"
     await bot.send_message(chat_id=chat_id, text=message)
 
+def schedule_daily_message():
+    schedule.every().day.at('00:17:05').do(asyncio.run, send_daily_message)
 
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-schedule.every().day.at('00:17:05').do(asyncio.run, send_daily_message())
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    schedule_daily_message()
+    run_schedule()
