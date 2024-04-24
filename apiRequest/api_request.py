4f36656a4db73d3d53762c63799de31edce6979a
dwijan <christianriveroarnez@gmail.com>
2024-04-24 18:22:31 -0400

    modified gemini prompt

--- apiRequest/api_request.py
+++ apiRequest/api_request.py
@@ -2,11 +2,12 @@ from dotenv import dotenv_values
 
 import google.generativeai as genai
 
+persons="7"
 def generate_recipe(plate):
     config = dotenv_values(".env")
     genai.configure(api_key=config["gemini_key"])
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content("Dame los ingredientes necesarios para hacer  " + plate + ". No necesito las proporciones ni como usarlos, solo la lista de ingredientes.")
+    response = model.generate_content("Dame los ingredientes necesarios para preparar  " + plate + " para "+persons+" personas. solo necesito la lista de ingredientes y las cantidades.")
     print(response.candidates[0].content.parts[0].text)
     return response.candidates[0].content.parts[0].text
 
