eac246dd73de7da0991cf13da0731c93094ad7bd
dwijan <christianriveroarnez@gmail.com>
2024-04-24 22:22:39 -0400

    fixed minor errors

--- bot.py
+++ bot.py
@@ -8,10 +8,12 @@ from dotenv import dotenv_values
 from database.firebaseFuncs import *
 import handleCommands.commandHandler as commandHandler
 import schedule
+import os
+from flask import Flask
 
 config = dotenv_values(".env")
 
-
+print("running chat", config["chat_id"])
 
 async def send_daily_message():
     try:
@@ -26,7 +28,7 @@ async def send_daily_message():
         await bot.send_message(chat_id=chat_id, text="No pude enviar el plato de hoy, si no registraste un menu este mes prueba usar el comando /generateMenu o agregalo tu con /setMenu")
 
 def schedule_daily_message():
-    schedule.every().day.at('09:00:00').do(run_async_in_thread, send_daily_message)
+    schedule.every().day.at('09:00:00',"America/La_Paz").do(run_async_in_thread, send_daily_message)
 
 def run_async_in_thread(coro):
     loop = asyncio.new_event_loop()
@@ -47,8 +49,19 @@ def main():
     application.add_handler(CommandHandler('generateMenu', commandHandler.generate_meals))
     application.run_polling(1.0)
     
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return 'OK'
+
+def run_flask_app():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == "__main__":
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.start()
     schedule_thread = threading.Thread(target=run_schedule)
     schedule_thread.start()
 
